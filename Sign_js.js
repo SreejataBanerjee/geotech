128d3972d0dcae78d5601588c14a99acbc85d876
SreejataBanerjee <112263429+SreejataBanerjee@users.noreply.github.com>
2024-03-14 20:11:10 +0530

    Add files via upload

---
+++ Sign_js.js
@@ -0,0 +1,10 @@
+console.log("Hemlo");
+
+function register() {
+  let email = document.getElementById("email").value;
+  let user = document.getElementById("user").value;
+  let password = document.getElementById("pass").value;
+  console.log(email);
+  console.log(user);
+  console.log(password);
+}
